4ac93f7639e9e0e1fab43e1d78f456f488923e20
Jandan <lutao1096@gmail.com>
2023-11-05 16:23:38 +0800

    更新

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'umi'
 export default defineConfig({
   npmClient: 'pnpm',
   presets: [require.resolve('@umijs/preset-vue')],
-  plugins: ['@umijs/plugins/dist/tailwindcss'],
+  plugins: ['@umijs/plugins/dist/tailwindcss', '@umijs/plugins/dist/request'],
   tailwindcss: {},
+  request: {},
 })
